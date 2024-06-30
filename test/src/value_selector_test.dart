c27e94b75e48eae7ed9ec679d9d686385db6b0cc
David Araujo <davidsdearaujo@gmail.com>
2024-06-30 12:00:11 -0300

    fix: - Listen other notifiers after the first value; - Do not emmit repeated values;

--- test/src/value_selector_test.dart
+++ test/src/value_selector_test.dart
@@ -5,50 +5,62 @@ import 'package:value_selectable/value_selectable.dart';
 void main() {
   test('ValueSelectable', () async {
     final counterState = ValueNotifier<int>(0);
-
-    final selectorState = ValueSelector<int>(
-      (get) => get(counterState) + 1,
-      (String action) {
-        if (action == 'INCREMENT') counterState.value++;
-        if (action == 'DECREMENT') counterState.value--;
-      },
-    );
+    final selectorState = ValueSelector<int>((get) => get(counterState) + 1);
 
     selectorState.addListener(expectAsync0(() {
       expect(selectorState.value, anyOf([2, 3]));
-    }, count: 3));
+    }, count: 2));
 
     // directly change
     counterState.value = 1;
+    counterState.value = 2;
 
-    // indirectly change
-    selectorState.value = 'INCREMENT';
-    selectorState.value = 'DECREMENT';
-
-    await Future.delayed(const Duration(seconds: 1));
+    // await Future.delayed(const Duration(seconds: 1));
     selectorState.dispose();
   });
 
-  test('ValueSelectable throw assert if reducer has more one arguments', () async {
-    expect(() {
-      return ValueSelector<int>(
-        (get) => 1,
-        (String action, int id) {},
-      );
-    }, throwsAssertionError);
-  });
-
   test('ValueSelectable action zero arguments', () async {
-    ValueSelector<int>(
-      (get) => 1,
-      () {},
-    );
+    ValueSelector<int>((get) => 1);
   });
 
   test('ValueSelectable action one arguments', () async {
-    ValueSelector<int>(
-      (get) => 1,
-      (String action) {},
-    );
+    final counterState = ValueNotifier<int>(0);
+    ValueSelector<int>((get) => get(counterState));
+  });
+
+  test('Listen other notifiers after the first value', () {
+    final nameState = ValueNotifier('Deivão');
+    final ageState = ValueNotifier(15);
+
+    final canAccess = ValueSelector((get) {
+      if (get(nameState) == 'Deivão') return true;
+      if (get(ageState) >= 18) return true;
+      return false;
+    });
+
+    canAccess.addListener(() => print('Can access: ${canAccess.value}'));
+
+    // Return false
+    nameState.value = "Jacob";
+
+    // Return true (doesn't work)
+    ageState.value = 18;
+  });
+
+  test('Do not emmit repeated values', () {
+    final nameState = ValueNotifier('Jacob');
+    final ageState = ValueNotifier(17);
+
+    final canAccess = ValueSelector((get) {
+      print('scope');
+      if (get(nameState) == 'Deivão') return true;
+      if (get(ageState) >= 18) return true;
+      return false;
+    });
+
+    canAccess.addListener(() => print('Can access: ${canAccess.value}'));
+
+    nameState.value = "Deivão";
+    ageState.value = 17;
   });
 }
